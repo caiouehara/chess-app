2427181103541db25b2ccebe8e866b8d586a6e62
caiouehara <caioueharamartins@gmail.com>
2022-02-18 17:07:22 -0300

    Add board

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const ctx = canvasElement.getContext("2d");
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 
+let state = {
+    isRunning: false,
+};
+
+let board = new Board(8, 8);
+  
 const draw = {
-    ball(ob) {
-        ctx.beginPath();
-        ctx.arc(ob.posX, ob.posY, ob.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = ob.color;
-        ctx.fill()
+    board(x, y){
+        for (var i = 0; i < x; i++) {
+            for (var j = 0; j < y; j++) {
+              ctx.beginPath();
+              ctx.strokeStyle = "black";
+              ctx.strokeRect(j * 80, i * 80, 80, 80);
+              ctx.closePath();
+            }
+          }
     },
     background(){
         ctx.fillStyle = "#999966";
@@ -17,29 +27,41 @@ const draw = {
     },
 }
 
-let state = {
-    isRunning: false,
-};
-
 function setup(){
     canvasElement.width = CANVAS_WIDTH;
     canvasElement.height = CANVAS_HEIGHT;
 
     isRunning = true;
 
-    loop()
+    loop();
 }
 
 function render(){
     draw.background()
+    draw.board(board.x, board.y);
 }
 
 function loop(){
     requestAnimationFrame(loop);
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    render()
+    render();
 }
 
+function Board(x, y) {
+    this.x = x;
+    this.y = y;
+    this.chartBoard = [];
+
+    for (var i = 0; i < this.x; i++) {
+        const row = [];
+        this.chartBoard.push(row);
+
+        for (var j = 0; j < this.y; j++) {
+            const col = [];
+            row.push(col);
+        }
+    }
+}
 
 //first call
 setup();
